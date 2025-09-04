56f297482a5607fd9b8406dcf61e4807cd19baf6
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-09-05 03:04:25 +0900

    라우터 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import SplashPage from "@/pages/SplashPage";
 import "@/styles/global.css";
 import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router";
 
 function App() {
-  return <SplashPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<SplashPage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
