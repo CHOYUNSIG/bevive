c2b3c00759df7e77b3026ff7c3d8a6775e0aa4db
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-09-05 01:18:16 +0900

    애니메이션 추가

--- src/layouts/DimLayout.tsx
+++ src/layouts/DimLayout.tsx
@@ -1,17 +1,18 @@
-import type { FC, ReactNode } from "react";
+import type { CSSProperties, FC, ReactNode } from "react";
 
-const DimLayout: FC<{ background: string; children: ReactNode }> = ({
-  background,
-  children,
-}) => {
+const DimLayout: FC<{
+  background?: CSSProperties["background"];
+  filter?: CSSProperties["filter"];
+  children: ReactNode;
+}> = ({ background, filter, children }) => {
   return (
-    <div
-      className="w-[100dvw] h-[100dvh] bg-cover bg-center"
-      style={{ backgroundImage: `url('${background}')` }}
-    >
-      <div className="w-full h-full bg-[url('/background.png')] bg-cover bg-center">
-        {children}
-      </div>
+    <div className="w-[100dvw] h-[100dvh] relative bg-black text-white">
+      <div
+        className="absolute w-full h-full bg-cover bg-center"
+        style={{ background, filter }}
+      />
+      <div className="absolute w-full h-full bg-[url('/background.png')] bg-cover bg-center" />
+      <div className="absolute w-full h-full">{children}</div>
     </div>
   );
 };
--- src/pages/SplashPage.tsx
+++ src/pages/SplashPage.tsx
@@ -1,31 +1,59 @@
 import DimLayout from "@/layouts/DimLayout";
+import { motion } from "framer-motion";
 import { type FC } from "react";
 
 const SplashPage: FC = () => {
   return (
-    <DimLayout background="/splash-background.png">
+    <DimLayout
+      background="url('/background-splash.png') no-repeat center/cover"
+      filter="blur(8px)"
+    >
       <div className="w-full h-full flex flex-col justify-center items-center px-4">
         <div className="relative mb-[30px]">
-          <img
-            src="/splash-logo.png"
-            alt="Bevive Logo"
-            className="w-[257.92px]"
-          />
-          <img
-            src="/splash-logo-outer.png"
-            alt="Bevive Logo"
-            className="w-full absolute top-0 left-0"
-          />
-          <img
-            src="/splash-logo-star.png"
-            alt="Bevive Logo"
-            className="w-full absolute top-0 left-0"
-          />
+          <motion.div
+            initial={{ opacity: 0, filter: "blur(8px)" }}
+            animate={{ opacity: 1, filter: "blur(0px)" }}
+            transition={{ duration: 0.5 }}
+          >
+            <img
+              src="/splash-logo.png"
+              alt="Bevive Logo"
+              className="w-[257.92px]"
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 3, delay: 0.2 }}
+          >
+            <img
+              src="/splash-logo-outer.png"
+              alt="Bevive Logo"
+              className="w-full absolute top-0 left-0"
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2, delay: 0.3 }}
+          >
+            <img
+              src="/splash-logo-star.png"
+              alt="Bevive Logo"
+              className="w-full absolute top-0 left-0"
+            />
+          </motion.div>
         </div>
-        <p className="text-base tracking-tighter m-0 text-[14px] leading-[13px] text-center">
-          Feel the vibe we create. <br />
-          Be the vibe we feel.
-        </p>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2, delay: 0.1 }}
+        >
+          <p className="text-base tracking-tighter m-0 text-[14px] leading-[13px] text-center">
+            Feel the vibe we create. <br />
+            Be the vibe we feel.
+          </p>
+        </motion.div>
       </div>
     </DimLayout>
   );
