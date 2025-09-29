9c651cbe31764d59fff204adc6d56c18abd5d22c
조윤식 <cho.yunsig56@gmail.com>
2025-09-29 21:10:16 +0900

    빌드 오류 해결

--- src/layouts/DimLayout.tsx
+++ src/layouts/DimLayout.tsx
@@ -2,16 +2,17 @@ import NavigationBar from "@/components/NavigationBar";
 import type { FC, ReactNode } from "react";
 
 const DimLayout: FC<{
-  background?: ReactNode; navigationBar?: {
-    onHomeButtonClicked: () => void,
-  }; children?: ReactNode;
+  background?: ReactNode;
+  navigationBar?: {
+    onHomeButtonClicked: () => void;
+    onSosButtonClicked: () => void;
+  };
+  children?: ReactNode;
 }> = ({ background, navigationBar, children }) => {
   return (
     <div className="w-[100dvw] h-[100dvh] relative bg-black text-white">
-      <div className="absolute w-full h-full">
-        {background}
-      </div>
-      <div className="absolute w-full h-full bg-[url('/background.png')] bg-cover bg-center"/>
+      <div className="absolute w-full h-full">{background}</div>
+      <div className="absolute w-full h-full bg-[url('/background.png')] bg-cover bg-center" />
       <div className="absolute w-full h-full">{children}</div>
       {navigationBar && (
         <div className="fixed bottom-[50px] left-[50%] -translate-x-1/2 mb-6">
