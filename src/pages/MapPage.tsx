e5bc11c14aed512ee0d7d7e2fd53f31b91d3bb4b
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-10-07 17:40:27 +0900

    요구사항 반영

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -42,13 +42,13 @@ const MapPage: FC = () => {
       navigationBar={navigationBar}
     >
       <div className="w-full h-full flex flex-row">
-        <div className="w-[308.62px] h-full flex flex-col justify-center items-center gap-[50px]">
+        <div className="w-[350px] h-full flex flex-col justify-center items-center gap-[50px] bg-black">
           <div className="w-full flex flex-col gap-2 items-center">
             <span className="font-['IDGrotesk'] text-[#F6FC00] text-[36px] font-normal leading-[60px]">
               Festival Map
             </span>
-            <div className="w-[246px] h-[38.4px] rounded-full bg-[#4B4B4B] flex flex-row gap-2 items-center px-2">
-              <div className="px-2">
+            <div className="w-[300px] h-[38.4px] rounded-full bg-[#4B4B4B] flex flex-row gap-2 items-center px-2">
+              <div className="w-full px-2">
                 <input
                   type="text"
                   className="w-full focus:outline-none placeholder:text-[14px] text-[16px]"
@@ -67,16 +67,20 @@ const MapPage: FC = () => {
           <div className="w-full px-[30px] flex flex-col gap-[20px]">
             <span className="text-[#C4C4C4] text-[16px]">주요 카테고리</span>
             <div className="group max-w-full flex flex-row flex-wrap gap-[6.5px]">
-              {mapOptions.map(({ name }) => {
+              {mapOptions.map(({ name }, index) => {
                 return (
                   <button
                     key={`button-${name}`}
-                    className="peer rounded-full text-[12px] font-medium h-[24.5px] flex justify-center items-center px-[22px] transition-all duration-200"
+                    className="peer rounded-full text-[12px] font-medium h-[24.5px] flex justify-center items-center px-[25px] transition-all duration-200"
                     onClick={() => setSearchWord(name)}
                     style={{
                       color: searchWord === name ? "black" : "#AEAEAE",
                       backgroundColor:
-                        searchWord === name ? "#F6FC00" : "#272727",
+                        searchWord === name
+                          ? "#F6FC00"
+                          : index === 0
+                          ? "#414141"
+                          : "#272727",
                     }}
                   >
                     {name}
