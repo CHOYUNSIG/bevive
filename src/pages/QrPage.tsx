b15e822352f0bf65405be3364a265f3b3ee19b90
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-10-07 13:25:41 +0900

    버그 패치

--- src/pages/QrPage.tsx
+++ src/pages/QrPage.tsx
@@ -11,7 +11,7 @@ const QrPage: FC = () => {
   const isIdle = useIdle({ timeout: 5000 });
 
   useEffect(() => {
-    if (isIdle) navigate(-1);
+    if (isIdle) navigate("/home");
   }, [isIdle, navigate]);
 
   return (
