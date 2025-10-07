3c3758f87a8bf9d72d090788b11975fa44c69556
CHOYUNSIG <cho.yunsig56@gmail.com>
2025-10-07 13:39:54 +0900

    애니메이션 추가

--- src/pages/YupYupWorkingPage.tsx
+++ src/pages/YupYupWorkingPage.tsx
@@ -1,6 +1,7 @@
 import ProgressiveStatusBar from "@/components/ProgressiveStatusBar";
 import useNavigationBar from "@/hooks/useNavigationBar";
 import DimLayout from "@/layouts/DimLayout";
+import { motion } from "framer-motion";
 import { useEffect, useState, type FC } from "react";
 import { useNavigate } from "react-router";
 
@@ -28,7 +29,20 @@ const YupYupWorkingPage: FC = () => {
     <DimLayout navigationBar={navigationBar}>
       <div className="w-full h-full flex flex-col justify-center items-center gap-[72px] pb-[10%]">
         <div className="relative">
-          <img src="/drone.png" className="w-[540px]" />
+          <motion.img
+            src="/drone.png"
+            className="w-[540px]"
+            animate={{
+              y: [0, -10, 0], // 0px -> -20px -> 0px 순으로 y축 이동
+            }}
+            // transition: 애니메이션의 전환 효과를 설정
+            transition={{
+              duration: 2, // 애니메이션 총 소요 시간
+              ease: "easeInOut", // 시작과 끝을 부드럽게
+              repeat: Infinity, // 무한 반복
+              repeatType: "mirror", // 갔다가 되돌아오는 움직임 (yoyo와 유사)
+            }}
+          />
           <span className="absolute bottom-0 left-1/2 -translate-x-[50%] text-[16px] font-medium">
             ⁘ 가까운{" "}
             <i className="bg-[url('/yupyup.svg')] bg-center bg-no-repeat bg-contain filter-[grayscale(1)] w-[80px] h-[12px] inline-block" />{" "}
